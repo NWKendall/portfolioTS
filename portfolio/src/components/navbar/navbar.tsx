348714bceb7d4c7994a01dcc07c26843c482b07d
nicbongo <nicbongo@protonmail.com>
2020-07-13 17:55:48 -0300

    now merging materialUI and raw CSS

--- portfolio/src/components/navbar/navbar.tsx
+++ portfolio/src/components/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import './navbar.styles.css';
+import "./navbar.styles.css";
+import { Tooltip } from "@material-ui/core";
 import EmailIcon from "@material-ui/icons/Email";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import FileCopyIcon from "@material-ui/icons/FileCopy";
@@ -7,13 +8,23 @@ import FileCopyIcon from "@material-ui/icons/FileCopy";
 const NavBar = () => {
   return (
     <div className="navbarContainer">
+      <h3 className="myName">Nic Kendall</h3>
       <nav className="navbar">
-        <h3 className="myName">Nic Kendall</h3>
-          <LinkedInIcon aria-label="LinkedIn" className="icon">
-            LinkedIn
-          </LinkedInIcon>
-          <FileCopyIcon className="icon">Resume</FileCopyIcon>
-          <EmailIcon target="__blank" href="www.google.com" />
+        <Tooltip title="www.linkedin.com/in/NWKendall">
+          <a className="icon" href="https://linkedin.com/in/nwkendall">
+            <LinkedInIcon aria-label="LinkedIn">LinkedIn</LinkedInIcon>
+          </a>
+        </Tooltip>
+        <Tooltip title="Click to download PDF">
+          <a className="icon">
+            <FileCopyIcon>Resume</FileCopyIcon>
+          </a>
+        </Tooltip>
+        <Tooltip title="nic.kendall.dev@protonmail.com">
+          <a href="mailto:nic.kendall.dev@protonmail.com" className="icon">
+            <EmailIcon />
+          </a>
+        </Tooltip>
       </nav>
     </div>
   );
